16e9a991e64dcae5f808bb6e7f1318f94a8701a9
m.aleksandrov <m.aleksandrov@avln.ru>
2026-06-10 16:04:45 +0300

    add docker compose

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 
-export $(grep -v '^#' .env | xargs)
-
 cd "${WORK_DIR}"
 git switch main
 git pull
-docker build --no-cache --build-arg BUILD_TIME="$(date -u +%Y-%m-%dT%H:%M:%SZ)" -t build-time-sender .
+#docker build --no-cache --build-arg BUILD_TIME="$(date -u +%Y-%m-%dT%H:%M:%SZ)" -t build-time-sender .
 #docker run --rm -e BOT_TOKEN="${BOT_TOKEN}" -e CHAT_ID="${CHAT_ID}" build-time-sender
-docker run -d --name build-time-sender -e BOT_TOKEN="${BOT_TOKEN}" -e CHAT_ID="${CHAT_ID}" build-time-sender
+#docker run -d --name build-time-sender -e BOT_TOKEN="${BOT_TOKEN}" -e CHAT_ID="${CHAT_ID}" build-time-sender
+
+BUILD_TIME="$(date -u +%Y-%m-%dT%H:%M:%SZ)" docker compose up build --no-cache
+docker compose up -d
+
