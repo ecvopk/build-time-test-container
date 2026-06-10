22e8c32117ec6ca6df9e7465a2b11688f755c206
m.aleksandrov <m.aleksandrov@avln.ru>
2026-06-10 15:38:41 +0300

    build container first...

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ export $(grep -v '^#' .env | xargs)
 cd "${WORK_DIR}"
 git switch main
 git pull
-docker run --rm -e BOT_TOKEN="${BOT_TOKEN}" -e CHAT_ID="${CHAT_ID}" build-time-sender
 docker build --build-arg BUILD_TIME="$(date -u +%Y-%m-%dT%H:%M:%SZ)" -t build-time-sender .
+docker run --rm -e BOT_TOKEN="${BOT_TOKEN}" -e CHAT_ID="${CHAT_ID}" build-time-sender
